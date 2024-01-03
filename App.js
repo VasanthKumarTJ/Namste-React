22a95c039626dea6de0374b76316ba39adcf2cec
VasanthKumarTJ <vasanthtj8@gmail.com>
2024-01-03 20:40:18 +0530

    Ep01 part-04 (nested element using react)

--- App.js
+++ App.js
@@ -1,5 +1,29 @@
-const heading = React.createElement("h1", {id:"heading"}, "hello world from react!");
+/* 
+<div id="parent">
+  <div id="child1">
+    <h1></h1>
+    <h2></h2>
+  </div>
 
+  <div id="child2">
+    <h1></h1>
+    <h2></h2>
+  </div>
+</div>
+*/
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "h1 tag from child1"),
+    React.createElement("h1", {}, "h1 tag from child1"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "h1 tag from child2"),
+    React.createElement("h1", {}, "h1 tag from child2"),
+  ]),
+]);
+
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading)
+root.render(parent);
