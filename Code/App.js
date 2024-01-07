902829c947af62aaa1b0330820ef76ebd4daf202
VasanthKumarTJ <vasanthtj8@gmail.com>
2024-01-07 19:12:17 +0530

    finish Ep-03 laying the foundation

--- Code/App.js
+++ Code/App.js
@@ -1,19 +1,33 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "h1 tag from child1"),
-    React.createElement("h1", {}, "h1 tag from child1"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "h1 tag from child2"),
-    React.createElement("h1", {}, "h1 tag from child2"),
-  ]),
-]);
+// react.createElement => object => HTMLelement(render)
+const parent = React.createElement("div", { id: "parent" }, "Hi from React");
 
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+//JSX - HTML like or XML like syntax
 
-root.render(parent);
+const Jsxelement = () => {
+  return <h1 id="header">Hi from JSX</h1>;
+  }
+  
+  const num = 5;
+  
+  const FunctionalComponent = () => {
+    return (
+      <div id="container">
+        {/* this is comment inside JSX */}
+         {/* calling another functional component  */}
+        <Jsxelement/>
+        {/* calling as a Javascript function - must have () as a function */}
+        {Jsxelement()}
+        {/* using expressions */}
+        {num}
+        {7 + 6}
+        <h1 id="functional">Hi from Functional Component</h1>
+      </div>
+    );
+  };
+
+   const root = ReactDOM.createRoot(document.getElementById("root"));
+  
+  root.render(<FunctionalComponent/>);
